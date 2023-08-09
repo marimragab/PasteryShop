67b6690fad19bacafb8bce1b3b463e962499e06d
marimragab <maryamragab@outlook.sa>
2023-08-10 01:30:14 +0300

    adding email tag helper functionality

---
+++ PasteryShop/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PasteryShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
