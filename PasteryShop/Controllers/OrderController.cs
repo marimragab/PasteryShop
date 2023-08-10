517352dc4bb1969394c10e24873908148f8e53b0
marimragab <maryamragab@outlook.sa>
2023-08-11 00:02:51 +0300

    adding order

---
+++ PasteryShop/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasteryShop.Services;
+
+namespace PasteryShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, IShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+    }
+}
