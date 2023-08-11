8b75190bb9b327422f88a47eb8c3c8ba187bc698
marimragab <maryamragab@outlook.sa>
2023-08-12 00:42:40 +0300

    adding unit tests

---
+++ PasteryShopTests/Controllers/PieControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using PasteryShop.Controllers;
+using PasteryShop.ViewModel;
+using PasteryShopTests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteryShopTests.Controllers
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void List_EmptyCategory_ReturnAllPies()
+        {
+            var mockPieRepository=RepositoryMocks.GetPieRepository();
+            var mockCategoryRepository=RepositoryMocks.GetCategoryRepository();
+            var pieController=new PieController(mockPieRepository.Object, mockCategoryRepository.Object);
+
+            var result = pieController.List("");
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel=Assert.IsAssignableForm<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+    }
+}
---
+++ PasteryShopTests/TagHelpers/EmailTagHelperTests.cs
@@ -0,0 +1,34 @@
+﻿using PasteryShop.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteryShopTests.TagHelpers
+{
+    public class EmailTagHelperTests
+    {
+        [Fact]
+        public void Generates_Email_Link()
+        {
+            EmailTagHelper emailTagHelper = new EmailTagHelper() { Address = "test@pasteryshop.com", Content = "Email" }; ;
+
+            var tagHelperContext = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(), string.Empty);
+
+            var content = new Mock<TagHelperContent>();
+
+            var tagHelperOutput = new TagHelperOutput("a",
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult(content.Object));
+
+            emailTagHelper.Process(tagHelperContext, tagHelperOutput);
+
+            Assert.Equal("Email", tagHelperOutput.Content.GetContent());
+            Assert.Equal("a", tagHelperOutput.TagName);
+            Assert.Equal("test@pasteryshop.com", tagHelperOutput.Attributes[0].Value);
+        }
+    }
+}
